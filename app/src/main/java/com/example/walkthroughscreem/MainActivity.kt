eb2fedc559f23fe3cc5f28b0b6259fc3fa2cbde4
Nurdev <nuriswanto66@gmail.com>
2023-04-21 10:08:34 -0700

    Model, RecyclerView, Adapter

--- app/src/main/java/com/example/walkthroughscreem/MainActivity.kt
+++ app/src/main/java/com/example/walkthroughscreem/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.walkthroughscreem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.viewpager2.widget.ViewPager2
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 
 class MainActivity : AppCompatActivity() {
+
+    var adapter:OnBoardingAdapter? = null
+    var list:ArrayList<OnBoardinglemModel>? = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        list!!.add(
+            OnBoardinglemModel(
+            R.drawable.one,
+            "Order Your Food",
+            "Now you can order food any time right from your mobile"
+        )
+        )
+
+        list!!.add(OnBoardinglemModel(
+            R.drawable.two,
+            "Cooking safe food",
+            "We are maintain safety and we keep clien while making food"
+        ))
+
+        list!!.add(OnBoardinglemModel(
+            R.drawable.three,
+            "Quick Delivery",
+            "Order your favorite meals will be immediately deliver"
+        ))
+
+        adapter = OnBoardingAdapter(list!!)
+        var viewPager = findViewById<ViewPager2>(R.id.view_pager)
+        viewPager.adapter = adapter
+        var wormIndicator = findViewById<WormDotsIndicator>(R.id.indicator)
+        wormIndicator.setViewPager2(viewPager)
+        var btnNext = findViewById<Button>(R.id.btn_next)
+        var btnSkip = findViewById<TextView>(R.id.tv_skip)
+        btnNext.setOnClickListener{
+            if (viewPager.currentItem+1 < adapter!!.itemCount){
+                viewPager.currentItem += 1
+            }else{
+                var intent = Intent(this@MainActivity,HomeActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        btnSkip.setOnClickListener {
+            var intent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/walkthroughscreem/OnBoardingAdapter.kt
+++ app/src/main/java/com/example/walkthroughscreem/OnBoardingAdapter.kt
@@ -1,27 +1,37 @@
 package com.example.walkthroughscreem
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class OnBoardingAdapter(var list:ArrayList<OnBoardinglemModel>) : RecyclerView.Adapter<OnBoardingAdapter.ViewHolder>() {
-    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+class OnBoardingAdapter(var list:ArrayList<OnBoardinglemModel>) :
+    RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+    class OnBoardingViewHolder(view : View) : RecyclerView.ViewHolder(view) {
         var image = view.findViewById<ImageView>(R.id.img)
         var title = view.findViewById<TextView>(R.id.tv_title)
         var desc = view.findViewById<TextView>(R.id.tv_desc)
+
+        fun bind(model:OnBoardinglemModel){
+            image.setImageResource(model.image)
+            title.text = model.title
+            desc.text = model.description
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
+        return OnBoardingViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        holder.bind(list[position])
     }
 }
